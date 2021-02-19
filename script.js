8da2b70feca2621d2dc9a92cc04365dcd9677359
bretpadlan <bret.padlan@ocsbstudent.ca>
2021-02-19 15:05:36 +0000

    js add spaces, remove single quotes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.getElementById('click').addEventListener ('click', agerr)
+document.getElementById('click').addEventListener('click', agerr)
 let age = 0
 
-function agerr() {
-age = document.getElementById('table').value
+function agerr () {
+  age = document.getElementById('table').value
   if (age >= 17) {
-   document.getElementById("name").innerHTML = 'You are allowed to see - XVII+'
+    document.getElementById('name').innerHTML = 'You are allowed to see - XVII+'
   } else if (age >= 13) {
-   document.getElementById("name").innerHTML = 'You are allowed to see - XIII+'
+    document.getElementById('name').innerHTML = 'You are allowed to see - XIII+'
   } else if (age >= 5) {
-   document.getElementById("name").innerHTML = 'You are allowed to see - V+'
+    document.getElementById('name').innerHTML = 'You are allowed to see - V+'
   } else {
-  document.getElementById("name").innerHTML = 'You are forbiden to see any movie, my child'
+    document.getElementById('name').innerHTML = 'You are forbiden to see any movie, my child'
   }
 }
